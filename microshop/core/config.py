0a4e2e6e655e5e33663e47f01ef54eb3a5b9d97a
Yaroslav <iaroslav.bierioza@gmail.com>
2025-09-17 13:06:19 +0300

    create DatabaseHelper

--- microshop/core/config.py
+++ microshop/core/config.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     db_url: str = "sqlite+aiosqlite:/// ./db.sqlite3"
+    # db_echo: bool = False
+    db_echo: bool = True
 
 
 settings = Settings()
