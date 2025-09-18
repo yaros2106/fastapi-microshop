c0a41c001ec2e83ca968f99b993eb0721b0becff
Yaroslav <iaroslav.bierioza@gmail.com>
2025-09-18 15:58:27 +0300

    throwing session into coroutine

--- microshop/core/models/db_helper.py
+++ microshop/core/models/db_helper.py
@@ -1,4 +1,12 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from asyncio import current_task
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
+)
 
 from core.config import settings
 
@@ -9,16 +17,36 @@ class DatabaseHelper:
         url: str,
         echo: bool = False,
     ):
-        self.engine = create_async_engine(
+        self.engine = create_async_engine(  # настройка движка
             url=url,
             echo=echo,
         )
-        self.session_factory = async_sessionmaker(
+        self.session_factory = async_sessionmaker(  # настройка сессии
             bind=self.engine,
             autoflush=False,
             autocommit=False,
             expire_on_commit=False,
         )
+
+    def get_scoped_session(self):  # создание сессии
+        session = async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task,
+        )
+        return session
+
+    async def session_dependency(
+        self,
+    ) -> AsyncGenerator[AsyncSession]:  # жизненный цикл конкретной сессии
+        session = self.get_scoped_session()  # создать сессию
+        async with session() as sess:  # отдать ее в роутер
+            yield sess
+            await session.remove()
+
+    # async def session_dependency(self):
+    #     session = self.get_scoped_session()
+    #     yield session
+    #     await session.close()
 
 
 db_helper = DatabaseHelper(
