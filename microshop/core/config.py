ff412181bcc3309f8cc724b0b965ca8021bbc471
Yaroslav <iaroslav.bierioza@gmail.com>
2025-09-23 13:27:40 +0300

    separation of pydantic settings

--- microshop/core/config.py
+++ microshop/core/config.py
@@ -1,13 +1,20 @@
 from pathlib import Path
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DB_PATH = BASE_DIR / "db.sqlite3"
+
+
+class DbSettings(BaseModel):
+    url: str = f"sqlite+aiosqlite:///{DB_PATH}"
+    echo: bool = False
+
 
 class Settings(BaseSettings):
-    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
-    db_echo: bool = False
+    db: DbSettings = DbSettings()
 
 
 settings = Settings()
