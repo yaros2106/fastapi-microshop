97a145e7d1526e595b0d1b2fffadbd410f0a48fb
Yaroslav <iaroslav.bierioza@gmail.com>
2025-09-23 13:26:53 +0300

    string representation of UserModel

--- microshop/models/user.py
+++ microshop/models/user.py
@@ -19,5 +19,12 @@ class UserModel(Base):
     __tablename__ = "users"
 
     username: Mapped[str] = mapped_column(String(32), unique=True)
+
     posts: Mapped[list["PostModel"]] = relationship(back_populates="user")
     profile: Mapped["ProfileModel"] = relationship(back_populates="user", uselist=False)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, username={self.username!r})"
+
+    def __repr__(self):
+        return str(self)
