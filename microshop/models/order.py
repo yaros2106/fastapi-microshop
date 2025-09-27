a48f7c8fcccab6ccbee3fbd14cd8ec6787631a33
Yaroslav <iaroslav.bierioza@gmail.com>
2025-09-27 17:36:12 +0300

    relationship between orders and products

--- microshop/models/order.py
+++ microshop/models/order.py
@@ -1,9 +1,18 @@
 from datetime import datetime, UTC
+from typing import TYPE_CHECKING
 
 from sqlalchemy import func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship,
+)
 
 from .base import Base
+from .order_product_association import order_product_association_table
+
+if TYPE_CHECKING:
+    from .product import ProductModel
 
 
 class OrderModel(Base):
@@ -15,4 +24,8 @@ class OrderModel(Base):
         default=lambda: datetime.now(
             UTC
         ),  # lambda оборачивает вызов, чтобы SQLAlchemy мог сам вызывать её при вставке
+    )
+    products: Mapped[list["ProductModel"]] = relationship(
+        secondary=order_product_association_table,
+        back_populates="orders",
     )
--- microshop/models/product.py
+++ microshop/models/product.py
@@ -1,6 +1,13 @@
-from sqlalchemy.orm import Mapped
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, relationship
 
 from .base import Base
+from .order_product_association import order_product_association_table
+
+
+if TYPE_CHECKING:
+    from .order import OrderModel
 
 
 class ProductModel(Base):
@@ -9,3 +16,8 @@ class ProductModel(Base):
     name: Mapped[str]
     description: Mapped[str]
     price: Mapped[int]
+
+    orders: Mapped[list["OrderModel"]] = relationship(
+        secondary=order_product_association_table,
+        back_populates="products",
+    )
