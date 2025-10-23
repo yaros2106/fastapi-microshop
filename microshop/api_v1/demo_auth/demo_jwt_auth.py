283540d15f9f9757e0d5715a0ed0591f48c35b6f
Yaroslav <iaroslav.bierioza@gmail.com>
2025-10-23 13:52:10 +0300

    auth user check self info though JWT

--- microshop/api_v1/demo_auth/demo_jwt_auth.py
+++ microshop/api_v1/demo_auth/demo_jwt_auth.py
@@ -5,10 +5,27 @@ from fastapi import (
     HTTPException,
     status,
 )
-
 from pydantic import BaseModel
+
+from fastapi.security import (
+    HTTPBearer,
+    HTTPAuthorizationCredentials,
+    OAuth2PasswordBearer,
+)
+from jwt.exceptions import InvalidTokenError
+
 from schemas.user_schema import UserSchema
 from auth import utils_jwt
+
+
+# http_bearer = HTTPBearer(
+#     scheme_name="JWT",
+#     description="Your **JWT token**",
+#     auto_error=False,
+# )
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/api/v1/demo-jwt/login",
+)
 
 
 class TokenInfo(BaseModel):
@@ -17,7 +34,7 @@ class TokenInfo(BaseModel):
 
 
 router = APIRouter(
-    prefix="/jwt",
+    prefix="/demo-jwt",
     tags=["JWT"],
 )
 
@@ -54,6 +71,11 @@ def validate_auth_user(
         hashed_password=user.password,
     ):
         return user
+    if not user.active:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="user not active",
+        )
     raise unauthed_exc
 
 
@@ -75,3 +97,54 @@ def auth_user_issue_jwt(
         access_token=token,
         token_type="Bearer",
     )
+
+
+def get_current_token_payload(
+    # credentials: HTTPAuthorizationCredentials = Depends(http_bearer),
+    token: str = Depends(oauth2_scheme),
+) -> dict:
+    # token = credentials.credentials
+    try:
+        payload = utils_jwt.decode_jwt(token=token)
+    except InvalidTokenError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"invalid token error",
+        )
+    return payload
+
+
+def get_current_auth_user(
+    payload: dict = Depends(get_current_token_payload),
+) -> UserSchema:
+    username: str | None = payload.get("sub")
+    if user := users_db.get(username):
+        return user
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="token invalid (user not found)",
+    )
+
+
+def get_current_active_user(
+    user: UserSchema = Depends(get_current_auth_user),
+) -> UserSchema:
+    if not user.active:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="user not active",
+        )
+    return user
+
+
+@router.get("/users/me")
+def auth_user_check_self_info(
+    payload: dict = Depends(get_current_token_payload),
+    user: UserSchema = Depends(get_current_active_user),
+):
+    iat = payload.get("iat")
+    return {
+        "username": user.username,
+        "email": user.email,
+        "logged_in_at": iat,
+    }
